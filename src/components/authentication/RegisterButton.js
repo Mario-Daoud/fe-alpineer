85ce76b03dea48d317992ca1f264acde93eb1dfa
Mario-Daoud <mario.daoud.3002@gmail.com>
2023-12-24 14:29:15 +0100

    update error handling on registerscreen

--- src/components/authentication/RegisterButton.js
+++ src/components/authentication/RegisterButton.js
@@ -1,69 +1,85 @@
-import React from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import PropTypes from "prop-types";
 import constants from "../../styles/constants";
 import { useAppContext } from "../../contexts/AppContext";
 import { API_URL } from "@env";
 
-const onRegisterPress = (props) => {
-  const { navigation, user: {username, password, confirmPassword }} = props;
-
-  if (password !== confirmPassword) {
-    console.log("Passwords do not match");
-    return;
-  }
-
-  fetch(`${API_URL}/users/register`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, password }),
-  })
-    .then((response) => {
-      if (response.ok) {
-        navigation.goBack();
-      } else {
-        console.log("Register failed");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
-
-onRegisterPress.propTypes = {
-  navigation: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired,
-};
 
 export default function RegisterButton(props) {
-  const { user, navigation } = props;
-  const { theme } = useAppContext();
+    const { user, navigation } = props;
+    const { theme } = useAppContext();
 
-  return (
-    <Pressable
-      style={[styles.registerButton, { backgroundColor: theme.PRIMARY }]}
-      onPress={() => onRegisterPress({ user, navigation })}
-    >
-      <Text style={styles.registerText}>Register</Text>
-    </Pressable>
-  );
+    const [isErrorVisible, setErrorVisible] = useState(false)
+
+    const showErrorMessage = () => {
+        setErrorVisible(true)
+
+        setTimeout(() => {
+            setErrorVisible(false)
+        }, 2000)
+    }
+
+    const onRegisterPress = (props) => {
+        const { navigation, user: { username, password, confirmPassword } } = props;
+
+
+        if (password !== confirmPassword) {
+            showErrorMessage()
+            return;
+        }
+
+
+        fetch(`${API_URL}/users/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    navigation.goBack();
+                } else {
+                    showErrorMessage()
+                }
+            })
+            .catch((error) => {
+                showErrorMessage()
+            });
+    };
+
+    onRegisterPress.propTypes = {
+        navigation: PropTypes.object.isRequired,
+        user: PropTypes.object.isRequired,
+    };
+
+    return (
+        <View>
+            <Pressable
+                style={[styles.registerButton, { backgroundColor: theme.PRIMARY }]}
+                onPress={() => onRegisterPress({ user, navigation })}
+            >
+                <Text style={styles.registerText}>Register</Text>
+            </Pressable>
+            {isErrorVisible && <Text style={{ color: "red" }}>Error while trying to register, please try again</Text>}
+        </View>
+    );
 }
 
 RegisterButton.propTypes = {
-  user: PropTypes.object.isRequired,
-  navigation: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+    navigation: PropTypes.object.isRequired,
 };
 
 const styles = StyleSheet.create({
-  registerButton: {
-    borderRadius: constants.BORDERRADIUS.MEDIUM,
-    padding: constants.MARGIN.MEDIUM,
-    alignItems: "center",
-  },
-  registerText: {
-    color: "white",
-    fontSize: constants.FONTSIZE.MEDIUM,
-  },
+    registerButton: {
+        borderRadius: constants.BORDERRADIUS.MEDIUM,
+        padding: constants.MARGIN.MEDIUM,
+        alignItems: "center",
+    },
+    registerText: {
+        color: "white",
+        fontSize: constants.FONTSIZE.MEDIUM,
+    },
 });
